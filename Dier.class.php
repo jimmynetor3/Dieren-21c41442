be44b17f8f07005e7db2ddaf5abf4f91a45dd467
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-07-14 01:24:13 +0200

    af

---
+++ Dier.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Dier
+{
+    public $dier;
+
+    public function eet()
+    {
+        echo($this->dier . " eet" . PHP_EOL);
+    }
+
+    public function slaap()
+    {
+        echo($this->dier . " slaapt" . PHP_EOL);
+    }
+
+}
---
+++ Vogel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "Dier.class.php";
+
+class Vogel extends Dier
+{
+    public function __construct()
+    {
+        $this->dier = "Vogel";
+    }
+
+    public function praat()
+    {
+        echo("tjilip tjilip");
+    }
+
+}
